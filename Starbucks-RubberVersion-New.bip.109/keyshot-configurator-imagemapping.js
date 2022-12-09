e13b6f0cde35a434def7102648d8b6a13b53e35d
DigiMax <42356489+DigiMaxIO@users.noreply.github.com>
2022-12-09 09:55:54 -0800

    Add files via upload

---
+++ Starbucks-RubberVersion-New.bip.109/keyshot-configurator-imagemapping.js
@@ -0,0 +1,17 @@
+imagemappingdata =
+[
+    {
+        "image": "Starbucks-RubberVersion-New.bip.109.1.png",
+        "modelsets": [
+            1005
+        ],
+        "studio": 665772112
+    },
+    {
+        "image": "Starbucks-RubberVersion-New.bip.109.2.png",
+        "modelsets": [
+            1064
+        ],
+        "studio": 665772112
+    }
+];
---
+++ Starbucks-RubberVersion-New.bip.109/keyshot-configurator-modeldata.js
@@ -0,0 +1,25 @@
+model =
+{
+    "materialgroups": [],
+    "products": [
+        {
+            "icon": "default.png",
+            "modelset": 1005,
+            "name": "Default"
+        },
+        {
+            "checked": true,
+            "icon": "model%20set%201.png",
+            "modelset": 1064,
+            "name": "Model Set 1"
+        }
+    ],
+    "studios": [
+        {
+            "checked": true,
+            "icon": "studio%201.png",
+            "id": 665772112,
+            "name": "Studio 1"
+        }
+    ]
+};
